691ccc70fa145af424d75eb41e71aa2a5d461e79
RizkiBimo09 <80391679+RizkiBimo09@users.noreply.github.com>
2021-04-04 22:50:33 +0700

    Add files via upload

---
+++ src/main/java/Tugas_1/Nomer_1.java
@@ -0,0 +1,14 @@
+package Tugas_1;
+public class Nomer_1 { 
+    public static void main (String[] args){
+        
+        int x,y;
+        for (x=0; x<=4; x++){
+            for (y=0; y<x; y++){
+                System.out.print(x);
+            }
+        System.out.println ();
+        }
+    }
+}
+
---
+++ src/main/java/Tugas_1/Nomer_2.java
@@ -0,0 +1,11 @@
+package Tugas_1;
+
+public class Nomer_2 {
+     public static void main(String[] args){
+        String[] siswa = {"Reinan", "Odena", "Geanno"}; //panjang array 3
+        
+            for (int i=0; i < siswa.length; i++){
+            System.out.println("Indeks ke " + i + " = " + siswa[i]);
+        }
+    }
+}
